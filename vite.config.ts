1795297e8632f1eb53e5d72dcc5d2d01e6281bcb
Michael Wybraniec <michaelwybraniec@me.com>
2025-05-16 23:57:46 +0200

    new config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
+const isGithubPages = process.env.GITHUB_PAGES === 'true'
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/todo/',
+  base: isGithubPages ? '/todo/' : '/',
   plugins: [
     react(),
     legacy()
